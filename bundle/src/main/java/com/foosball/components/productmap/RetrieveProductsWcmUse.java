7e596231ca4c16d19861ea0d68ed0b437fc67738
unknown <rammoole@INBLRNEEV099.global.publicisgroupe.net>
2015-06-25 11:45:32 +0530

    Adding hard coded image path.

--- bundle/src/main/java/com/foosball/components/productmap/RetrieveProductsWcmUse.java
+++ bundle/src/main/java/com/foosball/components/productmap/RetrieveProductsWcmUse.java
@@ -24,21 +24,26 @@ public class RetrieveProductsWcmUse extends WCMUse {
 		product1.setProductName("New ITSF B-ball");
 		product1.setPrice(100.34);
 		product1.setUrl("http://shop.foosball.com/newitsfbball.html");
+		product1.setImageloc("/content/dam/foosball-champs/new_itsf_b_ball.gif");
 		
 		Product product2 = new Product();
 		product2.setProductName("Tornado Tournament 3000 Table");
 		product2.setPrice(2099.00);
 		product2.setUrl("http://shop.foosball.com/toto30ta.html");
+		product2.setImageloc("/content/dam/foosball-champs/tornado-tournament-3000-table-11.jpg");
 		
 		Product product3 = new Product();
 		product3.setProductName("Red Soccer ball");
 		product3.setPrice(100.34);
 		product3.setUrl("http://shop.foosball.com/redsoccer.html");
+		product3.setImageloc("/content/dam/foosball-champs/red-soccer-3.jpg");
 		
 		Product product4 = new Product();
 		product4.setProductName("FoosGirl");
 		product4.setPrice(5.00);
 		product4.setUrl("http://shop.foosball.com/foosgirl.html");
+		product4.setImageloc("/content/dam/foosball-champs/foosgirl-4.jpg");
+		
 		productList.add(product1);
 		productList.add(product2);
 		productList.add(product3);
