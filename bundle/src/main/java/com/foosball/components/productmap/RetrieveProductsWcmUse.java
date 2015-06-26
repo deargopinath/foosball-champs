f18cca312739c4c6b8826347190412d72b446a5a
unknown <rammoole@INBLRNEEV099.global.publicisgroupe.net>
2015-06-26 10:33:43 +0530

    Adding Gift ideas related code while retrieveing products information.

--- bundle/src/main/java/com/foosball/components/productmap/RetrieveProductsWcmUse.java
+++ bundle/src/main/java/com/foosball/components/productmap/RetrieveProductsWcmUse.java
@@ -8,16 +8,17 @@ import javax.jcr.NodeIterator;
 import javax.jcr.Property;
 import javax.jcr.PropertyIterator;
 import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
 
-import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.scripting.SlingScriptHelper;
+import org.apache.sling.jcr.api.SlingRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.adobe.cq.sightly.WCMUse;
-import com.day.cq.wcm.api.Page;
 import com.foosball.services.common.utilities.Product;
 
 public class RetrieveProductsWcmUse extends WCMUse {
@@ -25,14 +26,56 @@ public class RetrieveProductsWcmUse extends WCMUse {
 	private final Logger logger = LoggerFactory.getLogger(RetrieveProductsWcmUse.class);
 	private List<Product> productList;
 
+	Session session;
+
 	@Override
 	public void activate() throws Exception {
+		logger.info("Inside activate method");
+		SlingScriptHelper sling = getSlingScriptHelper();
+		SlingRepository repository = sling.getService(SlingRepository.class);
+
+		session = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+		
+		String searchQuery = getCurrentPage().getName().toLowerCase();
+		QueryManager queryManager = session.getWorkspace().getQueryManager();
+		Query query = null;
+		if(searchQuery.contains("gift")) {
+			query = queryManager.createQuery("SELECT * FROM [nt:unstructured] AS s WHERE ISDESCENDANTNODE(s,'/content/FoosBallItems/jcr:content') AND s.price < 15", Query.JCR_SQL2);
+		} else {
+			query = queryManager.createQuery("SELECT * FROM [nt:unstructured] AS s WHERE ISDESCENDANTNODE(s,'/content/FoosBallItems/jcr:content') AND CONTAINS(s.title, '" + searchQuery + "')", Query.JCR_SQL2);
+		}
+		logger.info(query.getStatement());
+		QueryResult result = query.execute();
+		NodeIterator nodeIter = result.getNodes();
+		productList = new ArrayList<Product>();
+		while(nodeIter.hasNext()) {
+			Node node = nodeIter.nextNode();
+			Product product = new Product();
+			PropertyIterator propertyIter = node.getProperties();
+			while(propertyIter.hasNext()) {
+				Property prop = propertyIter.nextProperty();
+				if(!prop.getName().startsWith("jcr")) {
+					if(prop.getName().equals("title")) { 
+						product.setProductName(prop.getString());
+					} else if(prop.getName().equals("price")) {
+						product.setPrice(prop.getDecimal().doubleValue());
+					} else if(prop.getName().equals("description")) {
+						product.setDescription(prop.getString());
+					} else {
+						product.setImageloc(prop.getString());
+					}
+				}
+			}
+			productList.add(product);
+		}
 
 		// hard coding the products data here
 		
 		logger.info("inside activate method RetrieveProductWcmUse");
 		
 		/*productList = new ArrayList<Product>();
+		/*logger.info("inside activate method RetrieveProductWcmUse");
+		productList = new ArrayList<Product>();
 		Product product1 = new Product();
 		product1.setProductName("New ITSF B-ball");
 		product1.setPrice(100.34);
@@ -67,6 +110,8 @@ public class RetrieveProductsWcmUse extends WCMUse {
 
 			logger.info("Enter into getProductList");
 			productList = new ArrayList<Product>();
+		/*logger.info("Enter into getProductList");
+			List<Product> productList = new ArrayList<Product>();
 			try {
 				String searchQuery = " ";
 				Page page = getCurrentPage();
@@ -85,7 +130,7 @@ public class RetrieveProductsWcmUse extends WCMUse {
 
 				/*String componentPath = "/content/FoosBallItems/jcr:content"; //path to component
 				Node node = request.getResourceResolver().getResource(componentPath).adaptTo(Node.class);*/
-				Session session = request.getResourceResolver().adaptTo(Session.class);
+				/*Session session = request.getResourceResolver().adaptTo(Session.class);
 				
 				QueryManager queryManager = session.getWorkspace().getQueryManager();
 				
@@ -118,8 +163,8 @@ public class RetrieveProductsWcmUse extends WCMUse {
 			} catch (Exception e) {
 				logger.error("Error :::D>>" + e.getMessage());
 			}
-			logger.info("Size of Product list" + productList.size());
-			return productList;
+			logger.info("Size of Product list" + productList.size());*/
+		return productList;
 
 	}
 }
