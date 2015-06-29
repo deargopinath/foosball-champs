3d6f5c142b095f5247407607628d54b6460d0987
unknown <rammoole@INBLRNEEV099.global.publicisgroupe.net>
2015-06-29 12:21:31 +0530

    Adding Components

--- bundle/src/main/java/com/foosball/blog/wcm/components/RetrievePostsServlet.java
+++ bundle/src/main/java/com/foosball/blog/wcm/components/RetrievePostsServlet.java
@@ -1,5 +1,9 @@
 package com.foosball.blog.wcm.components;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
@@ -8,8 +12,13 @@ import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.foosball.blog.model.Post;
 import com.foosball.blog.service.PostService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 @Component(immediate=true, label="Retrieve Posts Component", metatype=true, description="This is a servlet to retrieve posts from JCR")
@@ -24,6 +33,7 @@ public class RetrievePostsServlet extends SlingSafeMethodsServlet {
 	 * Generated Serial Version UID
 	 */
 	private static final long serialVersionUID = -8107238707819821221L;
+	private final Logger logger = LoggerFactory.getLogger(RetrievePostsServlet.class);
 	
 	@Reference
 	PostService postService;
@@ -31,11 +41,18 @@ public class RetrievePostsServlet extends SlingSafeMethodsServlet {
 	@Override
 	public void doGet(SlingHttpServletRequest slingRequest, SlingHttpServletResponse slingResponse) {
 		String action = slingRequest.getParameter("action");
-		if(action.equals("trendingPosts")) {
-			// Code to fetch trending Posts from JCR
-			
-		} else {
-			// Code to fetch all posts from JCR
+		List<Post> postList = postService.getPostList(action);
+		GsonBuilder gsonbuilder = new GsonBuilder();
+		Gson gson = gsonbuilder.create();
+		String postListJson = gson.toJson(postList);
+
+		logger.info("Post List JSON :" + postListJson);
+		PrintWriter out;
+		try {
+			out = slingResponse.getWriter();
+			out.println(postListJson);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
